55093b350d6bdc43ac7e32686be779543312be64
Sonia Parker <soniaparker1207@gmail.com>
2025-12-20 11:01:23 +1100

    wire custom select state

--- src/components/common/ui/CustomSelect.jsx
+++ src/components/common/ui/CustomSelect.jsx
@@ -1,27 +1,34 @@
 import { formatTime } from "../../../utilities/dateFormat";
 import { hoursVals } from "../../../constants/hoursVals";
+import iconDropdown from "../../../assets/images/icon-dropdown.svg";
+import { useState } from "react";
 
 const CustomSelect = ({ hoursNum, days }) => {
   const currentWeekday = days.time[hoursVals.indexOf(hoursNum)];
-
+  const [selectOpen, setSelectOpen] = useState(false);
   return (
     <>
-      <div className="bg-neutral-600 relative text-sm font-medium rounded-lg">
-        <div className="flex gap-1 p-2">
+      <div className="relative text-sm font-medium">
+        <div
+          onClick={() => setSelectOpen(!selectOpen)}
+          className="flex gap-2 py-2 px-3 cursor-pointer bg-neutral-600 hover:bg-neutral-hover rounded-lg"
+        >
           {formatTime(currentWeekday, "weekOnly")}
-          {/* arrow icon here */}
-          <div> {hoursNum} </div>
+          <img src={iconDropdown} className={selectOpen ? "rotate-180" : "*:"}/>
         </div>
-        <div className="flex gap-2 flex-col rounded-lg p-3 absolute bottom-10 right-0 bg-neutral-800 border border-neutral-600">
-          {days.time.map((day, index) => (
-            <div key={index}>
-              {formatTime(day, "weekOnly")}
-              <span>-{index === 0 ? 8 : index * 24 + 8}</span>
-            </div>
-            // todo: wire on click func that will get hrs required value to pass to state
-            // todo: click on arrow to expand on click
-          ))}
-        </div>
+        {selectOpen && (
+          <div className="flex gap-1.5 flex-col rounded-lg py-3 absolute top-10 right-0 bg-neutral-800 border border-neutral-600">
+            {days.time.map((day, index) => (
+              <div 
+              key={index}
+              data-hrs={index === 0 ? 8 : index * 24 + 8}
+              className="hover:bg-neutral-700 py-1 px-3 cursor-pointer">
+                {formatTime(day, "weekOnly")}
+              </div>
+              // todo: wire on click func that will get hrs required value to pass to state
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
