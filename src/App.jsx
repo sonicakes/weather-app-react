747b8fb7f5a71d5a07b11efdea93f320b6c10e0a
Sonia Parker <soniaparker1207@gmail.com>
2025-12-21 11:20:04 +1100

    add tailwind to package lock

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import ContentWrapper from "./components/common/ContentWrapper";
-import crayons from "./assets/images/topographic.png";
+import topography from "./assets/images/topographic.png";
 
 const App = () => {
   return (
     <>
       <div className="relative h-screen">
-        <div className="mx-auto max-w-5xl px-6 lg:px-8 relative z-100">
+        <div className="mx-auto max-w-5xl px-6 lg:px-8 relative z-100 pb-8">
           <ContentWrapper />
         </div>
-        <img src={crayons} className="absolute top-0 left-0 w-full h-full z-50 opacity-30"/>
+        <img src={topography} className="absolute top-0 left-0 w-full h-full z-50 opacity-30"/>
       </div>
     </>
   );
